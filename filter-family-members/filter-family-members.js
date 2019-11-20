2ba5f87af2e4c7fa6bc2bc661cc5bfb0f9b757ff
mfmehdi <mfmehdi2@gmail.com>
2019-11-20 09:33:00 +0100

    Complete

--- filter-family-members/filter-family-members.js
+++ filter-family-members/filter-family-members.js
@@ -13,44 +13,59 @@
 //
 // EXAMPLE:
 //
-// var familyTree = {
-//   'firstName': 'Beth',
-//   'lastName': 'Johnson',
-//   'location': 'San Francisco',
-//   'children': [
-//     {
-//       'firstName': 'Beth Jr.',
-//       'lastName': 'Johnson',
-//       'location': 'Berkeley',
-//       'children': [
-//         {
-//           'firstName': 'Smitty',
-//           'lastName': 'Won',
-//           'location': 'Beijing',
-//           'children': []
-//         }
-//       ]
-//     },
-//     {
-//       'firstName': 'Joshie',
-//       'lastName': 'Wyattson',
-//       'location': 'Berkeley',
-//       'children': []
-//     }
-//   ]
-// };
+ //var familyTree = {
+ //  'firstName': 'Beth',
+ //  'lastName': 'Johnson',
+ //  'location': 'San Francisco',
+ //  'children': [
+ //    {
+ //      'firstName': 'Beth Jr.',
+ //      'lastName': 'Johnson',
+ //      'location': 'Berkeley',
+ //      'children': [
+ //        {
+ //          'firstName': 'Smitty',
+ //          'lastName': 'Won',
+ //          'location': 'Beijing',
+ //          'children': []
+ //        }
+ //      ]
+ //    },
+ //      {
+ //      'firstName': 'Joshie',
+ //      'lastName': 'Wyattson',
+ //      'location': 'Berkeley',
+ //      'children': []
+ //    }
+ //  ]
+ //};
 //
-// var livesInBerkeley = function (familyMember) {
-//   return familyMember.location === 'Berkeley';
-// }
-//
-// filterFamilyMembers(familyTree, livesInBerkeley)
+ //var livesInBerkeley = function (familyMember) {
+   //return familyMember.location === 'Berkeley';
+ //}
+
+
 //
 // returns ['Beth Jr. Johnson', 'Joshie Wyattson'];
 
 
-
+ 
 var filterFamilyMembers = function (familyTree, truthTest) {
   // All your code in this function body
+ var array=[];
+
+ truthTest(familyTree) && array.push(familyTree.firstName+" "+familyTree.lastName)
+
+  if(familyTree.children[0] === undefined){
+  	return array;
+  }
+
+  for (var i = 0; i < familyTree.children.length; i++) {
+  	array.push(...filterFamilyMembers(familyTree.children[i],truthTest))
+   }
+
+   return array;
 };
 
+
+ 
